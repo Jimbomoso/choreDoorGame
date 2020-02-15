701900d4d117ef1c92c6f28a18ec55c395023a35
jimbomoso <jimbomoso@gmail.com>
2020-02-15 17:57:57 -0500

    adds closeddoor variable and image path

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 
+const closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
+
+
 let numClosedDoors = 3;
 
 let openDoor1;
