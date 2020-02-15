9e35e51e309a50966f8b1352377a1a3897a5ce1f
jimbomoso <jimbomoso@gmail.com>
2020-02-15 17:28:50 -0500

    creates additional image variables and updates onclick events to each reflect a different image

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const doorImage2 = document.getElementById('door2');
 const doorImage3 = document.getElementById('door3');
 
 const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 
 doorImage1.onclick = () => {
     doorImage1.src = botDoorPath;
 };
 doorImage2.onclick = () => {
-    doorImage2.src = botDoorPath;
+    doorImage2.src = beachDoorPath
 };
 doorImage3.onclick = () => {
-    doorImage3.src = botDoorPath;
+    doorImage3.src = spaceDoorPath;
 };
 
 
