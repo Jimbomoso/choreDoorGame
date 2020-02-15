ed0a4882cd526f9e7757b6c33ca7a6e498969788
jimbomoso <jimbomoso@gmail.com>
2020-02-15 17:48:48 -0500

    Adds logic to randomchoregenerator function

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chor
 
 let numClosedDoors = 3;
 
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+
 doorImage1.onclick = () => {
     doorImage1.src = botDoorPath;
 };
@@ -19,5 +24,19 @@ doorImage3.onclick = () => {
 };
 
 const randomChoreGenerator = () => {
-
-
+    const choreDoor = Math.floor(Math.random() * numClosedDoors);
+    if (choreDoor === 0) {
+        openDoor1 = botDoorPath;
+        openDoor2 = beachDoorPath;
+        openDoor3 = spaceDoorPath;
+        return choreDoor;
+    } else if (choreDoor === 1) {
+        openDoor1 = spaceDoorPath;
+        openDoor2 = botDoorPath;
+        openDoor3 = beachDoorPath;
+    } else if (choreDoor === 2) {
+        openDoor1 = beachDoorPath;
+        openDoor2 = spaceDoorPath;
+        openDoor3 = botDoorPath;
+    }
+};
